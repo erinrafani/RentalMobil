53345a33e9b50590942fb714b2094c37c3ab03e5
erinrafani <erinrafani1223@gmail.com>
2021-11-16 18:42:42 +0700

    dasbord

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-hearder">
+                Larapus
+            </div>
+            <div class="card-body">
+                Selamat Datang Di <strong>Larapus</strong>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
